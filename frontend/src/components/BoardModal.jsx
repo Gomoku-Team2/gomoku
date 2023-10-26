13d0c20fcdef1297df22a36ceb90f8749a68d931
Dennis Eghtedari <year_2000_@hotmail.com>
2023-10-26 09:15:33 +0100

    fixed witdth

--- frontend/src/components/BoardModal.jsx
+++ frontend/src/components/BoardModal.jsx
@@ -12,7 +12,8 @@ function BoardModal() {
 export default BoardModal
 
 const BoardShadow = styled.div `
-width: 916px;
+width: 100%;
+max-width: 916px;
 height: 755px;
 border-radius: 50px;
 background: rgba(0, 0, 0, 0.34);
