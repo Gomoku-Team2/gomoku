ed74fc2586f25118412066d3eb25fc35232aead5
Emelie Widén <emelie.widen@icloud.com>
2023-10-26 09:48:01 +0200

    lagt till heroImg + namn + modal

---
+++ frontend/src/components/BoardModal.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+import styled from 'styled-components'
+
+function BoardModal() {
+  return (
+    <>
+<BoardShadow/>
+    </>
+  )
+}
+
+export default BoardModal
+
+const BoardShadow = styled.div `
+width: 916px;
+height: 755px;
+border-radius: 50px;
+background: rgba(0, 0, 0, 0.34);
+`
