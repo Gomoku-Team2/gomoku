250593eac664bf047472185004c56d7cf0f30d63
Dennis Eghtedari <year_2000_@hotmail.com>
2023-10-26 10:05:39 +0100

    added import in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'
 import HeroImg from './components/heroImg'
 import BoardModal from './components/BoardModal'
+import gameData from "../../backend/src/routes/game.json"
+import { useEffect } from 'react'
+
 
 function App() {
+  useEffect(() => {
+  console.log(gameData)
+  }, []);
 return (
 <section id='landing__page'>
 <div className='landing__page--content'>
