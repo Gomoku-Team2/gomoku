0217d241fa89a5e7b99f38f3c0737eefd3c69d84
Dennis Eghtedari <year_2000_@hotmail.com>
2023-10-30 12:51:32 +0000

    imported stones and fixed export in stones.jsx

--- frontend/src/components/Stones.jsx
+++ frontend/src/components/Stones.jsx
@@ -1,18 +1,21 @@
-import pinkStone from '../assets/image/Pink.png'
-import purpleStone from '../assets/image/Purple.png'
+import pinkStone from "../assets/image/Pink.png";
+import purpleStone from "../assets/image/Purple.png";
 
+export { pinkStone, purpleStone, stoneStyle };
 
 function Stones() {
   return (
     <div>
-    <img src={pinkStone} alt="pinkStone" style={stoneStyle} ></img>
-    <img src={purpleStone} alt="pinkStone" style={stoneStyle} ></img>
+      <img src={pinkStone} alt="pinkStone" style={stoneStyle}></img>
+      <img src={purpleStone} alt="pinkStone" style={stoneStyle}></img>
     </div>
   );
 }
 
-export default Stones
+export default Stones;
 const stoneStyle = {
-    width: '30px',
-    height: '30px',
-  };
+  width: "30px",
+  height: "30px",
+  position: "relative",
+  top: "3px",
+};
