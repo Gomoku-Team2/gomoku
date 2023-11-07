457337ca2fa4f6105f681f84339fd8d23334698f
Dennis Eghtedari <year_2000_@hotmail.com>
2023-11-07 21:20:20 +0000

    save

--- frontend/src/components/BoardData.jsx
+++ frontend/src/components/BoardData.jsx
@@ -1,11 +1,12 @@
 import gameData from "../../../backend/src/routes/game.json";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { pinkStone, purpleStone, stoneStyle } from "./Stones";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import WinnerMessage from "./WinnerMessage";
+import "./stone.css";
 
-function BoardData ({ updateScores, currentPlayer, handleCurrentPlayer  }) {
+function BoardData({ updateScores, currentPlayer, handleCurrentPlayer }) {
   const username1 = localStorage.getItem("Username1");
   const username2 = localStorage.getItem("Username2");
   //const [currentPlayer, setCurrentPlayer] = useState(player);
@@ -15,11 +16,13 @@ function BoardData ({ updateScores, currentPlayer, handleCurrentPlayer  }) {
   const [isClickEnabled] = useState(true);
   const resetBoard = gameData.board.tiles.map((row) => Array.from(row).fill(0));
   const [winningPlayer, setWinner] = useState(null);
+  const [winningSquares1, setWinningSquares1] = useState([]);
+  const [winningSquares2, setWinningSquares2] = useState([]);
 
   BoardData.propTypes = {
     updateScores: PropTypes.func.isRequired, // expects a function
     currentPlayer: PropTypes.func.isRequired, // expects a function
-    handleCurrentPlayer: PropTypes.func.isRequired // expects a function
+    handleCurrentPlayer: PropTypes.func.isRequired, // expects a function
   };
 
   function resetGame() {
@@ -27,20 +30,14 @@ function BoardData ({ updateScores, currentPlayer, handleCurrentPlayer  }) {
     setPlayer2Moves([]);
     handleCurrentPlayer(1);
     // setCurrentPlayer(1);
-    setWinner(null)
+    setWinner(null);
 
     setBoardState(resetBoard);
   }
 
-  function checkForWinner (){
-    if (checkWinCondition) {
-      setTimeout(() => {
-        const winningPlayer = currentPlayer === 1 ? username1 : username2;
-        setWinner(winningPlayer);
-        updateScores(currentPlayer);
-      }, 800);
-    }
-  }
+  useEffect(() => {
+    console.log(winningSquares1, winningSquares2);
+  }, []);
 
   const handleSquareClick = (rowIndex, colIndex) => {
     if (boardState[rowIndex][colIndex] === 0) {
@@ -61,6 +58,8 @@ function BoardData ({ updateScores, currentPlayer, handleCurrentPlayer  }) {
 
       setBoardState(updatedBoard);
       handleCurrentPlayer(currentPlayer === 1 ? 2 : 1);
+
+      checkForWinner();
     }
 
     function checkWinCondition(row, col, player) {
@@ -132,74 +131,37 @@ function BoardData ({ updateScores, currentPlayer, handleCurrentPlayer  }) {
     if (checkWinCondition(rowIndex, colIndex, currentPlayer)) {
       checkForWinner();
     }
+
+    function checkForWinner() {
+      const currentPlayerStones = currentPlayer === 1 ? player1Moves : player2Moves;
+      const winningSquares = [];
+      
+      for (const move of currentPlayerStones) {
+        const { row, col } = move;
+    
+        if (checkWinCondition(row - 1 , col - 1, currentPlayer)) {
+          const winner = currentPlayer === 1 ? username1 : username2;
+          updateScores(currentPlayer);
+          setWinner(winner);
+    
+          for (let i = 0; i < 5; i++) {
+            const square = { row: row - i, col: col - i };
+            if (i === 4) {
+              
+              square.height = 200;
+            }
+            winningSquares.push(square);
+          }
+        }
+      }
+    
+      if (currentPlayer === 1) {
+        setWinningSquares1(winningSquares);
+      } else {
+        setWinningSquares2(winningSquares);
+      }
+    }
   };
-
-  function checkWinCondition(row, col, player) {
-    let count = 1;
-    for (let i = col - 1; i >= 0 && boardState[row][i] === player; i--) {
-      count++;
-    }
-    for (
-      let i = col + 1;
-      i < boardState[row].length && boardState[row][i] === player;
-      i++
-    ) {
-      count++;
-    }
-    if (count >= 5) return true;
-
-    count = 1;
-    for (let i = row - 1; i >= 0 && boardState[i][col] === player; i--) {
-      count++;
-    }
-    for (
-      let i = row + 1;
-      i < boardState.length && boardState[i][col] === player;
-      i++
-    ) {
-      count++;
-    }
-    if (count >= 5) return true;
-
-    count = 1;
-    for (
-      let i = row - 1, j = col - 1;
-      i >= 0 && j >= 0 && boardState[i][j] === player;
-      i--, j--
-    ) {
-      count++;
-    }
-    for (
-      let i = row + 1, j = col + 1;
-      i < boardState.length &&
-      j < boardState[row].length &&
-      boardState[i][j] === player;
-      i++, j++
-    ) {
-      count++;
-    }
-    if (count >= 5) return true;
-
-    count = 1;
-    for (
-      let i = row - 1, j = col + 1;
-      i >= 0 && j < boardState[row].length && boardState[i][j] === player;
-      i--, j++
-    ) {
-      count++;
-    }
-    for (
-      let i = row + 1, j = col - 1;
-      i < boardState.length && j >= 0 && boardState[i][j] === player;
-      i++, j--
-    ) {
-      count++;
-    }
-    if (count >= 5) return true;
-
-    return false;
-  }
-
 
   return (
     <Wrapper>
@@ -215,9 +177,25 @@ function BoardData ({ updateScores, currentPlayer, handleCurrentPlayer  }) {
               }
             >
               {cell === 1 ? (
-                <img src={pinkStone} alt="" style={stoneStyle} />
+                <img
+                  src={pinkStone}
+                  alt=""
+                  style={
+                    winningPlayer === username1 && winningSquares1.some((square) => square.row === rowIndex && square.col === colIndex)
+                      ? { ...stoneStyle, height: "200px" }
+                      : stoneStyle
+                  }
+                />
               ) : cell === 2 ? (
-                <img src={purpleStone} alt="" style={stoneStyle} />
+                <img
+                  src={purpleStone}
+                  alt=""
+                  style={
+                    winningPlayer === username2 && winningSquares2.some((square) => square.row === rowIndex && square.col === colIndex)
+                      ? { ...stoneStyle, height: "200px" }
+                      : stoneStyle
+                  }
+                />
               ) : (
                 ""
               )}
@@ -225,7 +203,9 @@ function BoardData ({ updateScores, currentPlayer, handleCurrentPlayer  }) {
           ))
         )}
       </Container>
-      {winningPlayer && <WinnerMessage winner={winningPlayer} resetGame={resetGame} />}
+      {winningPlayer && (
+        <WinnerMessage winner={winningPlayer} resetGame={resetGame} />
+      )}
     </Wrapper>
   );
 }
